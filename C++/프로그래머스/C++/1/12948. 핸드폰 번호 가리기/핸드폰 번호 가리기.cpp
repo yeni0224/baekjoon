037f8a15cbb6fd66bbfa87687fbe77627cc931f4
yeni0224 <121008881+yeni0224@users.noreply.github.com>
2026-02-11 17:41:08 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ C++/프로그래머스/C++/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    for(int i = 0; i < phone_number.size(); i++)
+    {
+        if(i >= phone_number.size() - 4)
+        {
+            answer.push_back(phone_number[i]);
+        }
+        else
+        {
+            answer.push_back('*');
+        }
+    }
+    return answer;
+}
