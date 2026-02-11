d761cff185f1b138f795ac03c21bc7c7fe9b1321
yeni0224 <121008881+yeni0224@users.noreply.github.com>
2026-02-11 17:31:13 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.46 ms, Memory: 3.61 MB -BaekjoonHub

---
+++ C++/프로그래머스/C++/0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 2;
+    for(int i = 1; i < n; i++){
+     if(i * i == n){
+         answer = 1;
+     }
+    }
+    return answer;
+}
